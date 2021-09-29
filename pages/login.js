cb6079f9397d98d2c64c758742beae5f5885769a
BIswanath Prasad Singh <biswanathprasadsingh9@gmail.com>
2021-09-29 18:02:17 +0530

    '29SEP2021'

--- pages/login.js
+++ pages/login.js
@@ -48,7 +48,8 @@ export class login extends Component {
                     cookie.save('qtonixemailextractweb_userid', response.data.user._id, { path: '/',expires });
                     cookie.save('qtonixemailextractweb_userlogin',true, { path: '/',expires })
 
-                    window.location.href = process.env.appURL;
+                    
+					window.location.replace(process.env.appURL);
 
                 }else{
                     alert('Please check your email and password')
