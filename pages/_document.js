5c3ccac8c8ff5a56388722ca90cb17413538fdd6
BIswanath Prasad Singh <biswanathprasadsingh9@gmail.com>
2021-10-10 18:51:11 +0530

    '10OCT2021'

--- pages/_document.js
+++ pages/_document.js
@@ -11,21 +11,21 @@ class MyDocument extends Document {
       <Html>
         <Head>
         <link rel="stylesheet" type="text/css" href="/css/style.css" />
-		<link rel="stylesheet" type="text/css" href="/responsive.css" />
+		<link rel="stylesheet" type="text/css" href="/css/responsive.css" />
         </Head>
         <body>
           <Main />
           <NextScript />
 
-          {/* <script async src="/vendor/jquery.min.js" />
-          <script async src="/vendor/popper.js/popper.min.js" />
-          <script async src="/vendor/bootstrap/js/bootstrap.min.js" />
-          <script async src="/vendor/mega-menu/assets/js/custom.js" />
-          <script async src="/vendor/aos-next/dist/aos.js" />
-          <script async src="/vendor/jquery.appear.js" />
-          <script async src="/vendor/jquery.countTo.js" />
-          <script async src="/vendor/slick/slick.min.js" />
-          <script async src="/js/theme.js" /> */}
+          {/* <script src="/vendor/jquery.min.js" />
+          <script src="/vendor/popper.js/popper.min.js" />
+          <script src="/vendor/bootstrap/js/bootstrap.min.js" />
+          <script src="/vendor/mega-menu/assets/js/custom.js" />
+          <script src="/vendor/aos-next/dist/aos.js" />
+          <script src="/vendor/jquery.appear.js" />
+          <script src="/vendor/jquery.countTo.js" />
+          <script src="/vendor/slick/slick.min.js" />
+          <script src="/js/theme.js" /> */}
         </body>
       </Html>
     )
