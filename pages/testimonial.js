b61148402c61e1158fd495be2789b75801c0f893
Biswanath Prasad Singh <biswanathprasadsingh9@gmail.com>
2022-05-09 12:19:17 +0530

    '09MAY2022'

--- pages/testimonial.js
+++ pages/testimonial.js
@@ -17,6 +17,7 @@ class testimonial extends Component {
                         <div className="col-xl-9 col-lg-11 col-md-10 m-auto">
                             <div className="title-style-seven text-center">
                             <h2><span>Testi</span>monial</h2>
+                            <p>Get Email Addresses In Text, Web Pages, And Data Files. Using EMAIL EXTRACT Tool, You Will Be Able To Find A Lot Of Email Addresses To Send Emails To. The Application Lets You Extract Bulk Emails From Different Sources. You Can Extract Emails From Social Media, Like Facebook And Twitter, Websites Including Yelp, And Email Accounts.</p>
                             
                             {/* <p>We deliver an analytics experience with no hidden costs or additional products required. Understand the price you pay upfront, with no surprises. Buy our simple, capability-rich packages to upscale your business with verified emails.</p> */}
                             </div>
@@ -28,19 +29,15 @@ class testimonial extends Component {
 
         <div className='testimonial-main'>
             <div className="container">
-                <h3 className='mt-5'>Our Client&apos;s Say</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-                    magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                
 
                 <div className='testimonial_sec'>
 
                     <div className='row'>
-                        <div className="col-xl-2 m-auto">
+                        <div className="col-xl-2 m-auto order-1">
                             <img src='https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png' alt='email' className='w-50' />
                         </div>
-                        <div className="col-xl-10 m-auto">
+                        <div className="col-xl-10 m-auto order-2">
                             <p>&quot;Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
                             magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
                             consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
@@ -49,10 +46,27 @@ class testimonial extends Component {
                         </div>
                     </div>
                     <div className='row'>
-                        <div className="col-xl-2 m-auto">
+                    <div className="col-xl-10 m-auto order-2 order-lg-1">
+                            <p>&quot;Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
+                            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.&quot;</p>
+                            <h6>Adam Gibbs</h6>
+                        </div>
+                        <div className="col-xl-2 m-auto order-1 order-lg-2">
                             <img src='https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png' alt='email' className='w-50' />
                         </div>
-                        <div className="col-xl-10 m-auto">
+                        
+                    </div>
+
+
+
+
+                    <div className='row'>
+                        <div className="col-xl-2 m-auto order-1">
+                            <img src='https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png' alt='email' className='w-50' />
+                        </div>
+                        <div className="col-xl-10 m-auto order-2">
                             <p>&quot;Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
                             magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
                             consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
@@ -60,6 +74,24 @@ class testimonial extends Component {
                             <h6>Adam Gibbs</h6>
                         </div>
                     </div>
+                    <div className='row'>
+                    <div className="col-xl-10 m-auto order-2 order-lg-1">
+                            <p>&quot;Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
+                            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.&quot;</p>
+                            <h6>Adam Gibbs</h6>
+                        </div>
+                        <div className="col-xl-2 m-auto order-1 order-lg-2">
+                            <img src='https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png' alt='email' className='w-50' />
+                        </div>
+                        
+                    </div>
+
+
+
+
+                   
                 </div>
             </div>
         </div>
